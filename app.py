26483d918b551696f30ba4d5631defc9d03d1721
Wheberton Fialho <whebertonfialho@gmail.com>
2019-02-22 22:53:43 -0400

    Configurações API

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ def consultarCep():
 	return jsonify(json)
 
 if __name__ == '__main__':
-	#port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0')
+	port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
