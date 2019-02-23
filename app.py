74aeba3859cc33d3ce0c27b1723e95f918470ce1
Wheberton Fialho <whebertonfialho@gmail.com>
2019-02-22 22:29:17 -0400

    Alteração Config

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def consultarCep():
 
 if __name__ == '__main__':
 	#port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=5005)
+	app.run(host='0.0.0.0', port=os.environ.get('PORT', '5000'))
